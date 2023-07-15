cb1fbbb299142ed03f18c93e48a7cf9d68c5c7f3
DonHo0 <don.hoxha@polymaths.co>
2023-07-15 02:12:52 +0200

    Modifed the yml file

--- hello-world/src/App.test.tsx
+++ hello-world/src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders the App component', () => {
+    render(<App />);
+    
+    // Verify that the logo is rendered
+    const logoElement = screen.getByAltText('logo');
+    expect(logoElement).toBeInTheDocument();
+    
+    // Verify that the text "Edit src/App.tsx and save to reload." is present
+    const textElement = screen.getByText(/edit src\/App\.tsx and save to reload\./i);
+    expect(textElement).toBeInTheDocument();
+    
+    // Verify that the "Learn React" link is rendered with the correct URL
+    const linkElement = screen.getByRole('link', { name: /learn react/i });
+    expect(linkElement).toHaveAttribute('href', 'https://reactjs.org');
+  });
 });
