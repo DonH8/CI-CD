29731dc546e5df8626cb7c86496a1de11e77d9be
DonHo0 <don.hoxha@polymaths.co>
2023-07-15 18:21:13 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,16 +6,8 @@ describe('App', () => {
   test('renders the App component', () => {
     render(<App />);
     
-    // Verify that the logo is rendered
-    const logoElement = screen.getByAltText('logo');
-    expect(logoElement).toBeInTheDocument();
-    
     // Verify that the text "Edit src/App.tsx and save to reload." is present
     const textElement = screen.getByText(/edit src\/App\.tsx and save to reload\./i);
     expect(textElement).toBeInTheDocument();
-    
-    // Verify that the "Learn React" link is rendered with the correct URL
-    const linkElement = screen.getByRole('link', { name: /learn react/i });
-    expect(linkElement).toHaveAttribute('href', 'https://reactjs.org');
   });
 });
